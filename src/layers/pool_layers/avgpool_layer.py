41fe2b4b57e02be6e1ec2bc51bec3df480feac7c
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-11-13 17:55:30 +0300

    Update avgpool_layer.py

--- src/layers/pool_layers/avgpool_layer.py
+++ src/layers/pool_layers/avgpool_layer.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 
 class AvgPoolLayer(nn.Module):
-    def __init__(self, kernel_size=7):  # ResNet-50’de 7x7 feature map sonrası
+    def __init__(self, kernel_size=7):  
         super(AvgPoolLayer, self).__init__()
         self.pool = nn.AvgPool2d(kernel_size=kernel_size)
 
